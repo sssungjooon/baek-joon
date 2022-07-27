7039280415dc5a8c511b2a47a737b0fc24b7f830
sssungjooon <dellojoon7@gmail.com>
2022-07-27 20:01:00 +0900

    220727

---
+++ 백준 1000.py
@@ -0,0 +1,10 @@
+# A = int(input())
+# B = int(input())
+# A > 0
+# B < 10
+# print(A+B)
+
+A,B = map(int,input().split())
+A > 0
+B < 10
+print(A+B)
---
+++ 백준 10718.py
@@ -0,0 +1,2 @@
+result = '강한친구 대한육군\n'
+print(result*2)
